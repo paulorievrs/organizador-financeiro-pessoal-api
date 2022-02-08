955bd90f4e1a5f5720e977b2af2ed63967bea20c
Paulo Rievrs <paulorievrsoliveira@gmail.com>
2022-02-08 11:08:16 -0300

    Created database seeders and factories. Fixing some items on user model.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Expense;
+use App\Models\ExpenseTypes;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+
+        ExpenseTypes::insert([
+            [
+                'name' => 'fixed'
+            ],
+            [
+                'name' => 'variable'
+            ]
+
+        ]);
+
+        Expense::factory(10)->create();
+
     }
 }
